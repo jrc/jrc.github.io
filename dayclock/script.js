057a1db2f062f2891390fb946e54db95ed8ee74f
John Chang <mail@jrcpl.us>
2023-07-20 13:36:59 +0200

    Delete dead code

--- dayclock/script.js
+++ dayclock/script.js
@@ -9,41 +9,6 @@ document.addEventListener("DOMContentLoaded", function () {
         const now = new Date();
         const locale = navigator.language || navigator.userLanguage;
 
-        // // Polyfill the toLocaleTimeString method if it doesn't exist
-        // if (!Date.prototype.toLocaleTimeString) {
-        //     Date.prototype.toLocaleTimeString = function (locale, options) {
-        //         const date = this;
-        //         const format = options.hour12
-        //             ? 'h:mm:ss a'
-        //             : 'HH:mm:ss';
-        //         const formattedTime = date
-        //             .toString()
-        //             .split(' ')[4]
-        //             .replace(/:\d+ /, ' ');
-        //         return formattedTime;
-        //     };
-        // }
-
-        // // Polyfill the toLocaleDateString method if it doesn't exist
-        // if (!Date.prototype.toLocaleDateString) {
-        //     Date.prototype.toLocaleDateString = function (locale, options) {
-        //         const date = this;
-        //         const format = options.year === 'numeric'
-        //             ? 'MMMM d, yyyy'
-        //             : options.month === 'long'
-        //                 ? 'MMMM d'
-        //                 : options.day === 'numeric'
-        //                     ? 'd'
-        //                     : 'MM/dd/yyyy';
-
-        //         const formattedDate = format
-        //             .replace(/MMMM/, date.toLocaleString(locale, { month: 'long' }))
-        //             .replace(/yyyy/, date.toLocaleString(locale, { year: 'numeric' }))
-        //             .replace(/d/, date.toLocaleString(locale, { day: 'numeric' }));
-        //         return formattedDate;
-        //     };
-        // }
-
         const time = now.toLocaleTimeString(locale, { hour: 'numeric', minute: '2-digit' });
         const weekday = now.toLocaleDateString(locale, { weekday: 'long' });
         const date = now.toLocaleDateString(locale, { year: 'numeric', month: 'long', day: 'numeric' });
