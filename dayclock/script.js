502d3d3ef32e7286cff2f9469c900e9c6bdde4c3
John Chang <git@jrcpl.us>
2024-07-06 22:41:46 -0700

    Add error handler

--- dayclock/script.js
+++ dayclock/script.js
@@ -54,7 +54,6 @@ var FETCH_ANNOUNCEMENT_SECS = TEST_MODE ? 30 : 60 * 60 * 6; // 6 hours
 
 var testHoursAndMinutes = 0;
 
-
 const IS_OLD_BROWSER = (window.Intl === undefined);  // Workaround for Safari 9
 
 
@@ -66,7 +65,6 @@ function parseISODate(dateString) {
     const year = parseInt(parts[0], 10);
     const month = parseInt(parts[1], 10) - 1; // Months are 0-indexed
     const day = parseInt(parts[2], 10);
-  
     return new Date(year, month, day);
 }  
 
@@ -89,7 +87,7 @@ function describeDaysUntil(dateString) {
 }
 
   
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
     function fetchAnnouncement() {
         document.getElementById('announcement').textContent = ''; // Clear
 
@@ -117,117 +115,127 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function updateUI() {
-        const now = new Date();
-        var hourAndMinutes = now.getHours() + now.getMinutes() / 60.0;
+        try {
+            const now = new Date();
+            
+            var time;
+            var weekday;
+            var date;
 
-        var time;
-        var weekday;
-        var date;
+            if (IS_OLD_BROWSER) {
+                // toLocaleDate/TimeString(locale, ...) is not implemented
+                // and polyfill.io returns the time in UTC timezone
+                // so use Moment.js.
+                const now = moment();
+                time = now.format('LT');
+                weekday = now.format('dddd');
+                date = now.format('LL');
+            }
+            else {
+                // Known issue: This uses the browser locale, not the host's time/date format
+                // so e.g. with language='en-US', region=SE, it returns 12 not 24 hour time
+                time = now.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
+                weekday = now.toLocaleDateString([], { weekday: 'long' });
+                date = now.toLocaleDateString([], { year: 'numeric', month: 'long', day: 'numeric' });
+            }
 
-        if (IS_OLD_BROWSER) {
-            // toLocaleDate/TimeString(locale, ...) is not implemented
-            // and polyfill.io returns the time in UTC timezone
-            // so use Moment.js.
-            const now = moment();
-            time = now.format('LT');
-            weekday = now.format('dddd');
-            date = now.format('LL');
-        }
-        else {
-            // Known issue: This uses the browser locale, not the host's time/date format
-            // so e.g. with language='en-US', region=SE, it returns 12 not 24 hour time
-            time = now.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit' });
-            weekday = now.toLocaleDateString([], { weekday: 'long' });
-            date = now.toLocaleDateString([], { year: 'numeric', month: 'long', day: 'numeric' });
-        }
+            var hourAndMinutes = now.getHours() + now.getMinutes() / 60.0;
+            if (TEST_MODE) {
+                hourAndMinutes = testHoursAndMinutes;
+                time = hourAndMinutes + ':00 XX';
+            }
 
-        if (TEST_MODE) {
-            hourAndMinutes = testHoursAndMinutes;
-            time = hourAndMinutes + ':00 XX';
-        }
+            // For debugging:
+            // weekday = 'Wednesday';
+            // date = 'September 30, 2023';
+            // hourAndMinutes = 23;
 
-        // For debugging:
-        // weekday = 'Wednesday';
-        // date = 'September 30, 2023';
-        // hourAndMinutes = 23;
+            var themeClassName = 'theme-light';
+            var partofdayIcon;
+            var partofdayIconName = null;
+            var partofday;
 
-        var themeClassName = 'theme-light';
-        var partofdayIcon;
-        var partofdayIconName = null;
-        var partofday;
+            if (hourAndMinutes >= 7 && hourAndMinutes < 9) {
+                partofdayIcon = '🥣';
+                partofdayIconName = 'openmoji-72x72-color/1F963.png';
+                partofday = 'Breakfast';
+            }
+            else if (hourAndMinutes >= 12 && hourAndMinutes < 13) {
+                partofdayIcon = '🍚';
+                partofdayIconName = 'openmoji-72x72-color/1F35A.png';
+                partofday = 'Lunch';
+            }
+            else if (hourAndMinutes >= 17 && hourAndMinutes < 18.5) {
+                partofdayIcon = '🍜';
+                partofdayIconName = 'openmoji-72x72-color/1F35C.png';
+                partofday = 'Dinner';
+            }
+            else if (hourAndMinutes >= 6.5 && hourAndMinutes < 12) {
+                partofdayIcon = '';
+                partofday = 'Morning';
+            }
+            else if (hourAndMinutes >= 12 && hourAndMinutes < 18) {
+                partofdayIcon = '';
+                partofday = 'Afternoon';
+            }
+            else if (hourAndMinutes >= 18 && hourAndMinutes < 22) {
+                partofdayIcon = '';
+                partofday = 'Evening';
+            }
+            else {
+                themeClassName = 'theme-dark';
+                partofdayIcon = '🛌';
+                partofdayIconName = 'openmoji-72x72-color/1F6CC.png';
+                partofday = 'Sleep';
+            }
 
-        if (hourAndMinutes >= 7 && hourAndMinutes < 9) {
-            partofdayIcon = '🥣';
-            partofdayIconName = 'openmoji-72x72-color/1F963.png';
-            partofday = 'Breakfast';
-        }
-        else if (hourAndMinutes >= 12 && hourAndMinutes < 13) {
-            partofdayIcon = '🍚';
-            partofdayIconName = 'openmoji-72x72-color/1F35A.png';
-            partofday = 'Lunch';
-        }
-        else if (hourAndMinutes >= 17 && hourAndMinutes < 18.5) {
-            partofdayIcon = '🍜';
-            partofdayIconName = 'openmoji-72x72-color/1F35C.png';
-            partofday = 'Dinner';
-        }
-        else if (hourAndMinutes >= 6.5 && hourAndMinutes < 12) {
-            partofdayIcon = '';
-            partofday = 'Morning';
-        }
-        else if (hourAndMinutes >= 12 && hourAndMinutes < 18) {
-            partofdayIcon = '';
-            partofday = 'Afternoon';
-        }
-        else if (hourAndMinutes >= 18 && hourAndMinutes < 22) {
-            partofdayIcon = '';
-            partofday = 'Evening';
-        }
-        else {
-            themeClassName = 'theme-dark';
-            partofdayIcon = '🛌';
-            partofdayIconName = 'openmoji-72x72-color/1F6CC.png';
-            partofday = 'Sleep';
-        }
+            document.getElementById("container").className = "container";
+            document.getElementById("container").classList.add(themeClassName);
 
-        document.getElementById("container").className = "container";
-        document.getElementById("container").classList.add(themeClassName);
+            document.getElementById("top").className = "box top";
+            document.getElementById("top").classList.add('top' + partofday);
 
-        document.getElementById("top").className = "box top";
-        document.getElementById("top").classList.add('top' + partofday);
+            document.getElementById("middle").className = "box middle";
+            document.getElementById("middle").classList.add('middle' + partofday);
 
-        document.getElementById("middle").className = "box middle";
-        document.getElementById("middle").classList.add('middle' + partofday);
+            if (IS_OLD_BROWSER) {
+                const partofdayIconDiv = document.getElementById('partofdayIcon');
+                partofdayIconDiv.innerHTML = '';
+                if (partofdayIconName) {
+                    const img = document.createElement('img');
+                    img.src = partofdayIconName;
+                    partofdayIconDiv.appendChild(img); 
+                }
+            }
+            else {
+                document.getElementById('partofdayIcon').textContent = partofdayIcon;
+            }
 
-        if (IS_OLD_BROWSER) {
-            const partofdayIconDiv = document.getElementById('partofdayIcon');
-            partofdayIconDiv.innerHTML = '';
-            if (partofdayIconName) {
-                const img = document.createElement('img');
-                img.src = partofdayIconName;
-                partofdayIconDiv.appendChild(img); 
+            document.getElementById('partofday').textContent = partofday.toUpperCase();
+            document.getElementById('weekday').textContent = weekday;
+            document.getElementById('date').textContent = date;
+            document.getElementById('time').textContent = time.toUpperCase();
+
+            if (TEST_MODE) {
+                testHoursAndMinutes = ++testHoursAndMinutes % 24;
+                
+                setTimeout(updateUI, 1000 * TEST_TRANSITION_SECS);
+            }
+            else {
+                // Calculate the time remaining until the next full minute
+                const secondsRemaining = 60 - now.getSeconds();
+
+                // Schedule the next update at the next full minute
+                setTimeout(updateUI, 1000 * secondsRemaining);
             }
         }
-        else {
-            document.getElementById('partofdayIcon').innerHTML = partofdayIcon;
-        }
+        catch (err) {
+            console.error(err);
 
-        document.getElementById('partofday').innerHTML = partofday.toUpperCase();
-        document.getElementById('weekday').innerHTML = weekday;
-        document.getElementById('date').innerHTML = date;
-        document.getElementById('time').innerHTML = time.toUpperCase();
-
-        if (TEST_MODE) {
-            testHoursAndMinutes = ++testHoursAndMinutes % 24;
-            
-            setTimeout(updateUI, 1000 * TEST_TRANSITION_SECS);
-        }
-        else {
-            // Calculate the time remaining until the next full minute
-            const secondsRemaining = 60 - now.getSeconds();
-
-            // Schedule the next update at the next full minute
-            setTimeout(updateUI, 1000 * secondsRemaining);
+            const container = document.getElementById('container');
+            const newParagraph = document.createElement('p');
+            newParagraph.textContent = err;
+            container.appendChild(newParagraph);
         }
     }
 
